ee928fc806dd7a4ff13e0b459e5f2156f2b4da72
habibbehdarvandi <hb19.1381@gmail.com>
2021-07-20 02:39:00 +0430

    update 1

--- main.js
+++ main.js
@@ -3,3 +3,27 @@ const menu = document.querySelector('nav');
 menuBtn.addEventListener('click' , ()=>{
     menu.classList.toggle('show')
 })
+
+
+const dropdownItems = document.querySelectorAll('#dropdowns .item');
+
+dropdownItems.forEach(e => {
+    e.addEventListener('click' , (e)=>{
+
+        dropdownItems.forEach(e => {
+            if( e.classList.contains('active') ){
+                e.classList.toggle('active');
+            }else{
+                return;
+            }
+        });
+
+        let item = e.target;
+        if(!item.classList.contains('active')){
+            item.classList.toggle('active')
+        }else{
+            return;
+        }
+
+    });
+});
